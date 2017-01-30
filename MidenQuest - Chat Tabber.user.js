25f31537cbbb20b857a74dfaff8004501fe27f20
BobbyFischa <chriskolegraffwow@yahoo.com>
2017-01-30 11:24:16 -0700

    Update MidenQuest - Chat Tabber.user.js

--- MidenQuest - Chat Tabber.user.js
+++ MidenQuest - Chat Tabber.user.js
@@ -2,7 +2,7 @@
 // @name         MidenQuest - Chat Tabber
 // @version      1.0.0
 // @description  MidenQuest - Tab through chats
-// @author       Ryalane/Bobby
+// @author       Bobby
 // @include      http://www.midenquest.com/Game.aspx
 // @include      http://midenquest.com/Game.aspx
 // @grant        GM_xmlhttpRequest
@@ -11,42 +11,30 @@
 var _Chat = _Chat || {};
 _Chat.ShiftKey = false;
 _Chat.CurrentTab = 1;
+_Chat.Next = 0;
 
 _Chat.NextTab = function (Forward) {
-    var NextTab = 1;
-    var CurrentTabID = $('.TabSel').prop('id');
-    console.log(CurrentTabID);
-    var CurrentTab = CurrentTabID[CurrentTabID.length -1];
-
-    if(CurrentTab < 1 || CurrentTab > 5){
-        ChangeChatChannel(1); // If there's an error, default to public chat
-        _Chat.CurrentTab = 1;
-        console.log('ERROR - CurrentTab < 1 || CurrentTab > 5 ' + CurrentTab);
+    if(Forward){ // Tab to the right
+        // Kingdom Chat was added out of order. Tabbing left to right -> 1, 2, 5, 3, 4
+        if     (_Chat.CurrentTab == 1){_Chat.Next = 2;}
+        else if(_Chat.CurrentTab == 2){_Chat.Next = 5;}
+        else if(_Chat.CurrentTab == 5){_Chat.Next = 3;}
+        else if(_Chat.CurrentTab == 3){_Chat.Next = 4;}
+        else if(_Chat.CurrentTab == 4){_Chat.Next = 1;}
+        else                          {_Chat.Next = 1;}
     }
-    else
-    {
-        if(Forward){ // Tab to the right
-            // Kingdom Chat was added out of order. Tabbing left to right -> 1, 2, 5, 3, 4
-            if     (CurrentTab == 1){NextTab = 2;}
-            else if(CurrentTab == 2){NextTab = 5;}
-            else if(CurrentTab == 5){NextTab = 3;}
-            else if(CurrentTab == 3){NextTab = 4;}
-            else if(CurrentTab == 4){NextTab = 1;}
-            else                    {NextTab = 1;}
-        }
-        else{ // Tab to the left
-            // Kingdom Chat was added out of order. Tabbing right to left -> 4, 3, 5, 2, 1
-            if     (CurrentTab == 4){NextTab = 3;}
-            else if(CurrentTab == 3){NextTab = 5;}
-            else if(CurrentTab == 5){NextTab = 2;}
-            else if(CurrentTab == 2){NextTab = 1;}
-            else if(CurrentTab == 1){NextTab = 4;}
-            else                    {NextTab = 1;}
-        }
-
-        ChangeChatChannel(NextTab);
-        _Chat.CurrentTab = NextTab;
+    else{ // Tab to the left
+        // Kingdom Chat was added out of order. Tabbing right to left -> 4, 3, 5, 2, 1
+        if     (_Chat.CurrentTab == 4){_Chat.Next = 3;}
+        else if(_Chat.CurrentTab == 3){_Chat.Next = 5;}
+        else if(_Chat.CurrentTab == 5){_Chat.Next = 2;}
+        else if(_Chat.CurrentTab == 2){_Chat.Next = 1;}
+        else if(_Chat.CurrentTab == 1){_Chat.Next = 4;}
+        else                          {_Chat.Next = 1;}
     }
+
+    ChangeChatChannel(_Chat.Next);
+    _Chat.CurrentTab = _Chat.Next;
 };
 
 $(document).on('keyup keydown', function(e){_Chat.ShiftKey = e.shiftKey;}); // Detect shift press
@@ -61,5 +49,36 @@ $( document ).keydown(function(e) { // Handle tabbing / shift tabbing
     {
         _Chat.NextTab(true);
         e.preventDefault();
+    }
+});
+
+$("#ChatCh1").click(function () {
+    if (_Chat.CurrentTab != 1) {
+        _Chat.CurrentTab = 1;
+        ChangeChatChannel(1);
+    }
+});
+$("#ChatCh2").click(function () {
+    if (_Chat.CurrentTab != 2) {
+        _Chat.CurrentTab = 2;
+        ChangeChatChannel(2);
+    }
+});
+$("#ChatCh3").click(function () {
+    if (_Chat.CurrentTab != 3) {
+        _Chat.CurrentTab = 3;
+        ChangeChatChannel(3);
+    }
+});
+$("#ChatCh4").click(function () {
+    if (_Chat.CurrentTab != 4) {
+        _Chat.CurrentTab = 4;
+        ChangeChatChannel(4);
+    }
+});
+$("#ChatCh5").click(function () {
+    if (_Chat.CurrentTab != 5) {
+        _Chat.CurrentTab = 5;
+        ChangeChatChannel(5);
     }
 });
